6c3cdda667b3d0c9ae38686776b391ae660ae7a6
Teddy Lelong <teddy@webalgo.fr>
2022-04-30 13:11:12 +0200

    added Create a new User and Show User profile features

---
+++ snow_tricks/src/Controller/UserController.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\NewUserType;
+use App\Service\UserService;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+use DateTimeImmutable;
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/user/profile/{id<\d+>?1}", name="app_user_profile", methods={"GET"})
+     */
+    public function showProfile(User $user): Response
+    {
+        return $this->render('user/show-profile.html.twig', [
+            'user' => $user
+        ]);
+    }
+
+    /**
+     * @Route("/user/new", name="app_user_new")
+     */
+    public function new(Request $request, UserPasswordHasherInterface $userPasswordHasher, UserService $userService): Response
+    {
+        $user = new User();
+        $form = $this->createForm(NewUserType::class, $user);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setPassword(
+                $userPasswordHasher->hashPassword(
+                    $user,
+                    $form->get('password')->getData()
+                )
+            );
+            $user->setCreatedAt(new DateTimeImmutable());
+
+            $userService->add($user);
+
+            $this->addFlash('success', "Le nouvel utilisateur a été créé avec succès !");
+
+            return $this->redirectToRoute('app_user_new');
+        }
+        return $this->render('user/new.html.twig', [
+            'newUserType' => $form->createView()
+        ]);
+    }
+}
---
+++ snow_tricks/src/Form/NewUserType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class NewUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('username', TextType::class, [
+                'required' => true
+            ])
+
+            ->add('roles', ChoiceType::class, [
+                'required' => true,
+                'multiple' => false,
+                'expanded' => false,
+                'choices'  => [
+                    'Utilisateur'    => 'ROLE_USER',
+                    'Modérateur'     => 'ROLE_MODO',
+                    'Administrateur' => 'ROLE_ADMIN',
+                ]
+            ])
+
+            ->add('password', PasswordType::class, [
+                'required' => true
+            ])
+
+            ->add('email', EmailType::class, [
+                'required' => true
+            ])
+        ;
+
+        // Data transformer
+        $builder->get('roles')
+            ->addModelTransformer(new CallbackTransformer(
+                function ($rolesArray) {
+                    // transform the array to a string
+                    return count($rolesArray)? $rolesArray[0]: null;
+                },
+                function ($rolesString) {
+                    // transform the string back to an array
+                    return [$rolesString];
+                }
+            ));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
