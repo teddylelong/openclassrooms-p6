fbc52ec5029d8b0b785703474d3b478a4cc0c9dd
Teddy Lelong <teddy@webalgo.fr>
2022-05-07 15:04:55 +0200

    added FigureType Form

---
+++ snow_tricks/src/Form/FigureType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Figure;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class FigureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'invalid_message' => "Le nom d'une figure ne peut pas dépasser 45 caractères.",
+                'attr' => [
+                    'maxlength' => 45,
+                    'placeholder' => "Saisissez un nom",
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir un nom',
+                    ]),
+                ],
+            ])
+
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'placeholder' => "Décrivez la figure et donnez des indications sur la manière de pouvoir la réaliser",
+                ],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Veuillez saisir une description',
+                    ]),
+                ],
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Figure::class,
+        ]);
+    }
+}
