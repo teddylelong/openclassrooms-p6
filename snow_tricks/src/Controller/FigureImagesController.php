a1130b5a7305607e39295740fefdde27347ea853
Teddy Lelong <teddy@webalgo.fr>
2022-05-17 19:24:40 +0200

    added delete a figure image feature

---
+++ snow_tricks/src/Controller/FigureImagesController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\FigureImages;
+use App\Repository\FigureImagesRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FigureImagesController extends AbstractController
+{
+    /**
+     * @Route("/figure/images/delete/{id<\d+>}", name="app_figure_image_delete")
+     */
+    public function delete(Request $request, FigureImages $image, FigureImagesRepository $figureImagesRepository)
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if ($this->isCsrfTokenValid('delete'.$image->getId(), $data['_token'])) {
+            $fileName = $image->getFilename();
+            unlink($this->getParameter('images_directory').'/'.$fileName);
+
+            $figureImagesRepository->remove($image);
+
+            return new JsonResponse(['success' => 1]);
+        } else {
+            return new JsonResponse(['error' => 'Token Invalide'], 400);
+        }
+    }
+}
